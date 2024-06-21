e72176ebf946a46a834a21937ad17d1c28a0bd85
vikasqblocks <vikas@qblocks.cloud>
2024-06-21 09:48:56 +0000

    Add chainlit llama index cookbook

---
+++ MonsterLLM_ChainLit_LLamaIndex/Readme.md
@@ -0,0 +1,13 @@
+# MonsterAPI GA LLM Chatbot with chainlit and llamaindex
+
+## Dependencies
+
+```bash
+pip install -r requirements.txt
+```
+
+## Run Service
+
+```bash
+python3 -m uvicorn app:app
+```
---
+++ MonsterLLM_ChainLit_LLamaIndex/app.py
@@ -0,0 +1,66 @@
+import os
+import openai
+import chainlit as cl
+import numpy as np
+
+from llama_index.core import (
+    Settings,
+    StorageContext,
+    VectorStoreIndex,
+    SimpleDirectoryReader,
+    load_index_from_storage,
+)
+from llama_index.llms.openai import OpenAI
+from llama_index.embeddings.openai import OpenAIEmbedding
+from llama_index.core.query_engine.retriever_query_engine import RetrieverQueryEngine
+from llama_index.core.callbacks import CallbackManager
+from llama_index.core.service_context import ServiceContext
+
+from monsterapi.LLamaIndexClient import MonsterLLM
+
+PERSIST_DIR = "/etc/llamaparse/storage"
+DOCUMENT_DIR = "./data"
+
+if not os.path.exists(PERSIST_DIR):
+    # load the documents and create the index
+    documents = SimpleDirectoryReader(DOCUMENT_DIR).load_data(show_progress=True)
+    index = VectorStoreIndex.from_documents(documents)
+    # store it for later
+    index.storage_context.persist(persist_dir=PERSIST_DIR)
+else:
+    # load the existing index
+    storage_context = StorageContext.from_defaults(persist_dir=PERSIST_DIR)
+    index = load_index_from_storage(storage_context)
+
+
+@cl.on_chat_start
+async def start():
+    
+    model = "meta-llama/Meta-Llama-3-8B-Instruct"
+    Settings.llm = MonsterLLM(
+        model=model, temperature=0.1, max_tokens=256
+    )
+    
+    Settings.embed_model = OpenAIEmbedding(model="text-embedding-3-small")
+    Settings.context_window = 2048
+
+    service_context = ServiceContext.from_defaults(callback_manager=CallbackManager([cl.LlamaIndexCallbackHandler()]))
+    query_engine = index.as_query_engine(streaming=True, similarity_top_k=2, service_context=service_context)
+    cl.user_session.set("query_engine", query_engine)
+
+    await cl.Message(
+        author="Assistant", content="Hello! Im an FinMuni a assistant focussed on Indian Finance still learnign ropes. How may I help you?"
+    ).send()
+
+
+@cl.on_message
+async def main(message: cl.Message):
+    query_engine = cl.user_session.get("query_engine") # type: RetrieverQueryEngine
+
+    msg = cl.Message(content="", author="Assistant")
+    
+    res = await cl.make_async(query_engine.query)(message.content)
+
+    for token in res.response_gen:
+        await msg.stream_token(token)
+    await msg.send()
